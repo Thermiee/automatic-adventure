fb587586b11407656b87adcbb7d33d46bd31711d
Thermiee <navyblack16@yahoo.com>
2022-11-14 22:41:50 +0100

    create main and article views

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,16 @@
+const styles = {
+    margin: "1em",
+    padding: "1em",
+    border: "1px solid #ddd"
+  };
+  
+  const Main = props => {
+    return (
+      <div style={styles}>
+        <center>
+          <h1>React RoR Blog</h1>
+        </center>
+        <ArticlesContainer />
+      </div>
+    );
+  };
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do 
+    namespace :v1 do 
+     resources :articles, only: [:index, :create, :destroy, :update]
+    end 
+  end 
+  root to: 'home#index'
 end
